a60478d549583397f17cd278ce2e5dd66066aea5
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2017-10-16 22:29:34 +0200

    Bot token params are now env vars

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-ruby '2.4.1' 
+ruby '>= 2.4.1'
 source 'https://rubygems.org'
 
 gem 'discordrb', :git => 'https://github.com/meew0/discordrb.git'
--- botMain.rb
+++ botMain.rb
@@ -1,7 +1,8 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new(token: 'MzY5NTY2MDk0NzMxNzcxOTE0.DMaYww.QNVz7fFQLcq60F-jWHdI2XrdoUA', client_id: 369566094731771914)
-#To make the bot join a guild https://discordapp.com/oauth2/authorize?&client_id=369566094731771914&scope=bot 
+bot = Discordrb::Bot.new(token: ENV['BOT_TOKEN'], client_id: ENV['BOT_CLIENT_ID'])
+#To make the bot join a guild https://discordapp.com/oauth2/authorize?&client_id=369566094731771914&scope=bot
+
 bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
 end
